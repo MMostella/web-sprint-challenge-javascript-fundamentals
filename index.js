70fd8d7b128d38c7cfcfe641ab72bc4a5538a74a
MMostella <masonmostella@gmail.com>
2021-07-23 10:06:32 -0500

    17 passed, 7 failed

--- index.js
+++ index.js
@@ -29,7 +29,18 @@ console.log(`Task 1: nestedFunction can access internal variable because that th
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
 function summation(number) {
-  // return number + (number - 1) + (number - 2) + (number - 3);
+  const array = [];
+  const len = Math.ceil(Math.log10(number + 1));
+  // for (let i = 0; i < len; i++){
+  //   array.push[i];
+  // }
+  return len;
+  // const num1 = (number - 3);
+  // const num2 = (number - 2);
+  // const num3 = (number - 1);
+  // const array = [num1, num2, num3, number];
+  // return num1 + num2 + num3 + number;
+
   // var count = 0;
   // for (let i = 0; i < number.length; i++){
   //   return count = count + number[1];
@@ -37,7 +48,7 @@ function summation(number) {
 }
   
  
-  // console.log('Task 2:', summation(4));
+  console.log('Task 2:', summation(1222220));
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
@@ -153,10 +164,10 @@ function greeting(first, last){
 /* 🐴🐴🐴 Step 1: Base Constructor 🐴🐴🐴
  Use the constructor function named CuboidMaker to accept properties for length, width, and height which can be initialized as an object
 */
-function CuboidMaker(len, wid, hei){
-  this.len = len;
-  this.wid = wid;
-  this.hei = hei;
+function CuboidMaker(att){
+  this.len = att.len,
+  this.wid = att.wid,
+  this.hei = att.hei
 }
 
 
@@ -168,6 +179,9 @@ CuboidMaker.prototype.volume = function(){
   return (this.len * this.wid * this.hei);
 }
 
+// CuboidMaker.prototype.volume = function(){
+//   return (${this.len} * ${this.wid} * ${this.hei});
+// }
 
 /* 🐴🐴🐴 Step 3: Surface Area Method 🐴🐴🐴
   Create another method called surfaceArea using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
@@ -175,16 +189,22 @@ CuboidMaker.prototype.volume = function(){
   2 * (length * width + length * height + width * height)  */
 
 CuboidMaker.prototype.surfaceArea = function(){
-  return (2 * (this.len * this.wid + this.len * this.hei + this.wid * this.hei));
+  return (2 * ((this.len * this.wid) + (this.len * this.hei) + (this.wid * this.hei)));
 }
 
-
+// CuboidMaker.prototype.surfaceArea = function(){
+//   return (2 * (${this.len} * ${this.wid} + ${this.len} * ${this.hei} + ${this.wid} * ${this.hei}));
+// }
 
 /* 🐴🐴🐴 Step 4: Create a new object that uses CuboidMaker 🐴🐴🐴
   Create an object called cuboid that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid. */
 
-const cuboid = new CuboidMaker(4, 5, 5);
+const cuboid = new CuboidMaker({
+  len: 4,
+  wid: 5,
+  hei: 5
+});
 
 // 🐴🐴🐴 Test your volume and surfaceArea methods by uncommenting the logs below: 🐴🐴🐴
 // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
@@ -197,24 +217,24 @@ console.log('Topic 3:', cuboid.surfaceArea()); // 130
 
 class CuboidMakerTwo{
   constructor(att){
-    this.len = att.len;
-    this.wid = att.wid;
-    this.hei = att.hei;
+    this.length = att.length;
+    this.width = att.width;
+    this.height = att.height;
   }
 
   volume(){
-    return (this.len * this.wid * this.hei);
+    return (this.length * this.width * this.height);
   }
 
   surfaceArea(){
-    return (2 * (this.len * this.wid + this.len * this.hei + this.wid * this.hei));
+    return (2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height)));
   }
 }
 
 const cuboidTwo = new CuboidMakerTwo({
-  len: 4,
-  wid: 5,
-  hei: 5
+  length: 4,
+  width: 5,
+  height: 5
 });
 
 
@@ -228,10 +248,27 @@ console.log('Topic 4:', cuboidTwo.surfaceArea()); // 130
 
 // 🦄 💪 Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area. 🦄 💪
   
+class CubeMaker extends CuboidMaker {
+  constructor(att){
+    super(att);
+    this.side = att.side
+  }
 
+  volume(){
+    return (this.side * 3);
+  }
 
+  surfaceArea(){
+    return (6 * (this.side * 2));
+  }
+}
 
+const cube = new CubeMaker({
+  side: 3
+});
 
+console.log('Stretch:', cube.volume());
+console.log('Stretch:', cube.surfaceArea());
 
   /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
   function foo(){
